baf20d3a2ce3b68acadd54502f5f342cffbba2cc
SASIKANTBISWAL <biswalsashikant@gmail.com>
2020-06-22 18:08:32 +0530

    Implemneted selection and insertion sort.

--- Sorting/src/com/insertionsort/InsertionSort.java
+++ Sorting/src/com/insertionsort/InsertionSort.java
@@ -1,10 +1,48 @@
 package com.insertionsort;
 
+import java.util.Scanner;
+
 public class InsertionSort {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-
+		System.out.println("Enter the size of the array : ");
+		int size =  sc.nextInt();
+		
+		int arr[] = new int[size];
+		
+		System.out.println("Enter the elements of the array : ");
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.println("Array elements before sorting");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print("["+arr[i]+"] ");
+		}
+		
+		System.out.println();
+		
+		//Insertion sort logic  
+		for(int i=1;i<arr.length;i++) {
+			int key = arr[i];
+			int j = i - 1;
+			
+			while(j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
+		
+		System.out.println("Array elements after sorting");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print("["+arr[i]+"] ");
+		}
+		
+		System.out.println();
+		sc.close();
 	}
 
 }
--- Sorting/src/com/selectionsort/SelectionSort.java
+++ Sorting/src/com/selectionsort/SelectionSort.java
@@ -1,10 +1,50 @@
 package com.selectionsort;
 
+import java.util.Scanner;
+
 public class SelectionSort {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-
+		System.out.println("Enter the size of the array : ");
+		int size = sc.nextInt();
+		
+		int arr[] = new int[size];
+		
+		System.out.println("Enter the elements of the array : ");
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.println("Array elements before sorting");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print("["+arr[i]+"] ");
+		}
+		
+		System.out.println();
+		
+		//Selection sort logic
+		for(int i=0;i<arr.length-1;i++) {
+			int min_index = i;
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[j] < arr[min_index]) {
+					min_index = j;
+				}
+			}
+			int temp = arr[min_index];
+			arr[min_index] = arr[i];
+			arr[i] = temp;
+		}
+		
+		System.out.println("Array elements after sorting");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print("["+arr[i]+"] ");
+		}
+		
+		System.out.println();
+		
+		sc.close();
 	}
 
 }
